75cebddd9f39d7b3f4caa8f678d1dbc8ed1e9f9c
Jacek <kucjac@gmail.com>
2018-04-25 17:37:46 +0200

    First try for gorm repository.

--- repositories/gorm-repository.go
+++ repositories/gorm-repository.go
@@ -7,6 +7,7 @@ import (
 	"github.com/kucjac/jsonapi"
 	"github.com/kucjac/uni-db"
 	"github.com/kucjac/uni-db/gormconv"
+	"reflect"
 	"strings"
 )
 
@@ -46,12 +47,13 @@ func (g *GORMRepository) initialize(db *gorm.DB) (err error) {
 }
 
 func (g *GORMRepository) buildScopeQuery(scope *jsonapi.Scope,
-) (query string, values []interface{}, err error) {
+) (db *gorm.DB, err error) {
 	gormScope := g.db.NewScope(scope.Value)
 	mStruct := gormScope.GetModelStruct()
-
 	var columnName string
 	var operator string
+
+	// every filter field is new search.Where
 	for _, filter := range scope.Filters {
 		var gormField *gorm.StructField
 		switch filter.Field.GetJSONAPIType() {
@@ -74,28 +76,53 @@ func (g *GORMRepository) buildScopeQuery(scope *jsonapi.Scope,
 					continue
 				}
 			}
-			// how to build from relationships?
-			// it need subscopes
+
 		default:
 			err = fmt.Errorf("Unsupported jsonapi field type: '%v' for field: '%s' in model: '%v'.", filter.Field.GetJSONAPIType(), filter.Field.GetFieldName(), scope.Struct.GetType())
-			return err
+			return nil, err
 		}
 
 		if gormField == nil {
 			err = fmt.Errorf("Invalid filtering field: '%v' not found in the gorm ModelStruct: '%v'", filter.Field.GetFieldName(), mStruct.ModelType)
-			return err
+			return nil, err
 		}
 		columnName = gormField.DBName
-		if len(filter.Values) == 1 {
-			switch filter.Values[0].Operator {
-
+		if filter.Field.GetJSONAPIType() == jsonapi.Attribute || filter.Field.GetJSONAPIType() == jsonapi.Primary {
+			err := addQueryToScope(filter, gormScope)
+		} else {
+			// no direct getter for table name
+			tempScope := gormScope.New(reflect.New(filter.Field.GetRelatedModelType()))
+			tempScope.SelectAttrs
+			for _, rel := range filter.Relationships {
+				err := addQueryToScope(rel, tempScope)
+				if err != nil {
+					return err
+				}
 			}
+			tempScope.Search.Select(tempScope.PrimaryField().DBName)
+			expr := tempScope.DB().QueryExpr()
+			gormScope.Search.Where(columnName, expr)
 		}
-
-		// relacja powinna być subquery
-
 	}
-	return nil, nil
+	return gormScope.DB(), nil
+}
+
+func addQueryToScope(filter *jsonapi.FilterScope, scope *gorm.Scope) error {
+	for _, fv := range filter.Values {
+		op := sqlizeOperator(fv.Operator)
+		var valueMark string
+		if fv.Operator == jsonapi.OpIn || fv.Operator == jsonapi.OpNotIn {
+			valueMark = "(?)"
+		} else {
+			if len(fv.Values) > 1 {
+				err = fmt.Errorf("Too many values for given operator: '%s', '%s'", fv.Values, fv.Operator)
+				return err
+			}
+			valueMark = "?"
+		}
+		q := fmt.Sprintf("%s %s %s", columnName, op, valueMark)
+		scope.Search.Where(q, fv.Values...)
+	}
 }
 
 func sqlizeOperator(operator jsonapi.FilterOperator) string {
